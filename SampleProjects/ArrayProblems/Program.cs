5771926bb72289b62e5b752f087b9a99886e2ba6
Sairam Madem <sairam.madem@gmail.com>
2018-10-09 14:16:45 -0700

    Sample projects.

---
+++ SampleProjects/ArrayProblems/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] input = new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } };
+
+            OpenOrSenior(input);
+        }
+
+        public static IEnumerable<string> OpenOrSenior(int[][] data)
+        {
+            List<string> output = new List<string>();
+
+            if (data == null || data.Length == 0)
+            {
+                throw new ArgumentNullException("data is null or empty.");
+            }
+
+            int length = data.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                int[] temp = data[i];
+
+                if(temp == null || temp.Length != 2)
+                {
+                    throw new ArgumentNullException($"data is not proper. " +
+                        $"{i}th sub-array is null or length is more than two.");
+                }
+
+                if(temp[0] >= 55 && temp[1] > 7)
+                {
+                    output.Add("Senior");
+                    continue;
+                }
+
+                output.Add("Open");
+            }
+
+            return output;
+        }
+    }
+}
---
+++ SampleProjects/AsyncDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task t = Task.Run(() => LongRunningTask());
+            // t.Wait();
+
+            LongRunningTask();
+            Console.WriteLine("Long running task is finished.");
+        }
+
+        static void LongRunningTask()
+        {
+            int temp = 1;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(temp++);
+
+                if (temp == 1000)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
